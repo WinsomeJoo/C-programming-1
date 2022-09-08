6ea03bee4c57956bcd206f71b34eb382c6c83a47
WinsomeJoo <112889287+WinsomeJoo@users.noreply.github.com>
2022-09-08 17:03:12 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+
+	printf("Input two integer: ");
+	scanf("%d %d",&a,&b);
+
+	printf("%d & %d=%d\n", a, b, a & b);
+	printf("%d | %d=%d\n", a, b, a | b);
+	printf("%d ^ %d=%d\n", a, b, a ^ b);
+
+	
+}
+
