f0e211c2e6ae0574858dc27ea331c3b339684524
WinsomeJoo <112889287+WinsomeJoo@users.noreply.github.com>
2022-09-15 14:48:01 +0900

    Add files via upload

---
+++ 3주차 .c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	double km = 0.0, mile;
+	
+	printf("plese enter kilometers:");
+	scanf("%lf", &km);
+	
+	mile = km / 1.609;
+
+	printf("%.1f km is equl to %.1f miles\n", km, mile);
+
+}
+
